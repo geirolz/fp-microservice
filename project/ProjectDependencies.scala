55834612cf2d438f4157a177f3eab9c95803d50a
geirolz <david.geirolz@gmail.com>
2021-06-29 00:09:04 +0200

    Refactoring ProjectDependencies

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -3,26 +3,59 @@ import sbt._
 object ProjectDependencies {
 
   lazy val common: Seq[ModuleID] = Seq(
-    //SCALA
-    "org.typelevel" %% "cats-core" % "2.6.1",
-    "org.typelevel" %% "cats-effect" % "2.5.1",
-    //HTTP
-    "org.http4s" %% "http4s-dsl" % "0.21.24",
-    "org.http4s" %% "http4s-blaze-server" % "0.21.24",
-    //TAPIR
-    "com.softwaremill.sttp.tapir" %% "tapir-core" % "0.17.19",
-    "com.softwaremill.sttp.tapir" %% "tapir-http4s-server" % "0.17.19",
-    "com.softwaremill.sttp.tapir" %% "tapir-json-circe" % "0.17.19",
-    "com.softwaremill.sttp.tapir" %% "tapir-openapi-docs" % "0.17.19",
-    "com.softwaremill.sttp.tapir" %% "tapir-openapi-circe-yaml" % "0.17.19",
-    "com.softwaremill.sttp.tapir" %% "tapir-swagger-ui-http4s" % "0.17.19",
-    //JSON
-    "io.circe" %% "circe-core" % "0.14.1",
-    //CONFIG
-    "com.github.pureconfig" %% "pureconfig" % "0.16.0",
-    //LOGGING
+    effects,
+    logging,
+    config,
+    db,
+    http,
+    json
+  ).flatten
+
+  private val effects = {
+    Seq(
+      "org.typelevel" %% "cats-core" % "2.6.1",
+      "org.typelevel" %% "cats-effect" % "2.5.0"
+    )
+  }
+
+  private val logging = Seq(
     "org.typelevel" %% "log4cats-slf4j" % "1.3.1",
     "org.slf4j" % "slf4j-api" % "1.7.31",
     "org.slf4j" % "slf4j-simple" % "1.7.31"
+  )
+
+  private val config = Seq(
+    "com.github.pureconfig" %% "pureconfig" % "0.16.0"
+  )
+
+  private val db = {
+    val doobieVersion = "0.12.1"
+    Seq(
+      "org.tpolecat" %% "doobie-core" % doobieVersion,
+      "org.tpolecat" %% "doobie-h2" % doobieVersion,
+      "org.tpolecat" %% "doobie-scalatest" % doobieVersion % "test"
+    )
+  }
+
+  private val http = {
+    val http4sVersion = "0.21.24"
+    val tapirVersion = "0.17.19"
+
+    Seq(
+      //HTTP
+      "org.http4s" %% "http4s-dsl" % http4sVersion,
+      "org.http4s" %% "http4s-blaze-server" % http4sVersion,
+      //TAPIR
+      "com.softwaremill.sttp.tapir" %% "tapir-core" % tapirVersion,
+      "com.softwaremill.sttp.tapir" %% "tapir-http4s-server" % tapirVersion,
+      "com.softwaremill.sttp.tapir" %% "tapir-json-circe" % tapirVersion,
+      "com.softwaremill.sttp.tapir" %% "tapir-openapi-docs" % tapirVersion,
+      "com.softwaremill.sttp.tapir" %% "tapir-openapi-circe-yaml" % tapirVersion,
+      "com.softwaremill.sttp.tapir" %% "tapir-swagger-ui-http4s" % tapirVersion
+    )
+  }
+
+  private val json = Seq(
+    "io.circe" %% "circe-core" % "0.14.1"
   )
 }
