05c79985484a1e12afecfcb46e2ecb4faa01de5b
Scala Steward <me@scala-steward.org>
2021-11-06 02:22:41 +0100

    Update tapir-cats, tapir-core, ... to 0.19.0-M14

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -27,7 +27,7 @@ object ProjectDependencies {
 
   private val http = {
     val http4sVersion = "0.23.6"
-    val tapirVersion  = "0.19.0-M13"
+    val tapirVersion  = "0.19.0-M14"
 
     Seq(
       // HTTP
