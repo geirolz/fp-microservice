6722f1fe427447afe443b213482c929c252a64d9
Scala Steward <me@scala-steward.org>
2021-12-30 22:48:22 +0100

    Update cats-effect to 3.3.2

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -22,7 +22,7 @@ object ProjectDependencies {
   private val effects =
     Seq(
       "org.typelevel" %% "cats-core" % "2.7.0",
-      "org.typelevel" %% "cats-effect" % "3.3.1"
+      "org.typelevel" %% "cats-effect" % "3.3.2"
     )
 
   private val config = {
