773fdd1d9775475819ecc3daf8795b8b1be86d50
David Geirola <david.geirola@gmail.com>
2022-07-31 17:20:41 +0200

    Refactoring ProjectDependencies

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -2,11 +2,16 @@ import sbt._
 
 object ProjectDependencies {
 
+  val catsVersion       = "2.8.0"
+  val catsEffectVersion = "3.3.14"
   val http4sVersion     = "0.23.14"
   val tapirVersion      = "1.0.3"
   val pureConfigVersion = "0.17.1"
   val doobieVersion     = "1.0.0-RC2"
   val fs2RabbitVersion  = "5.0.0"
+  val refinedVersion    = "0.10.1"
+  val circeVersion      = "0.14.2"
+  val slf4Version       = "1.7.36"
 
   lazy val common: Seq[ModuleID] = Seq(
     general,
@@ -24,15 +29,15 @@ object ProjectDependencies {
 
   private val general = Seq(
     "com.github.geirolz" %% "scope-core" % "0.0.5",
-    "eu.timepit" %% "refined" % "0.10.1",
-    "eu.timepit" %% "refined-cats" % "0.10.1",
-    "eu.timepit" %% "refined-pureconfig" % "0.10.1"
+    "eu.timepit" %% "refined" % refinedVersion,
+    "eu.timepit" %% "refined-cats" % refinedVersion,
+    "eu.timepit" %% "refined-pureconfig" % refinedVersion
   )
 
   private val effects =
     Seq(
-      "org.typelevel" %% "cats-core" % "2.8.0",
-      "org.typelevel" %% "cats-effect" % "3.3.14"
+      "org.typelevel" %% "cats-core" % catsVersion,
+      "org.typelevel" %% "cats-effect" % catsEffectVersion
     )
 
   private val config = {
@@ -62,9 +67,9 @@ object ProjectDependencies {
     )
 
   private val json = Seq(
-    "io.circe" %% "circe-core" % "0.14.2",
-    "io.circe" %% "circe-generic-extras" % "0.14.2",
-    "io.circe" %% "circe-refined" % "0.14.2"
+    "io.circe" %% "circe-core" % circeVersion,
+    "io.circe" %% "circe-generic-extras" % circeVersion,
+    "io.circe" %% "circe-refined" % circeVersion
   )
 
   private val metrics =
@@ -74,8 +79,8 @@ object ProjectDependencies {
 
   private val logging = Seq(
     "org.typelevel" %% "log4cats-slf4j" % "2.4.0",
-    "org.slf4j" % "slf4j-api" % "1.7.36",
-    "org.slf4j" % "slf4j-simple" % "1.7.36"
+    "org.slf4j" % "slf4j-api" % slf4Version,
+    "org.slf4j" % "slf4j-simple" % slf4Version
   )
 
   private val tests = Seq(
