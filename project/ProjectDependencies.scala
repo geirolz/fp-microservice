db8bc716944eabc6a8c052ebd1bfd6b3c0b03310
David Geirola <david.geirola@gmail.com>
2022-12-28 15:59:11 +0100

    Fix formatting

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -94,7 +94,7 @@ object ProjectDependencies {
   private val db =
     Seq(
       // migrations
-      "com.github.geirolz" %% "fly4s-core" % "0.0.14",
+      "com.github.geirolz" %% "fly4s-core" % "0.0.15+12-6e94a041+20221228-0028-SNAPSHOT",
 
       // management
       "org.tpolecat" %% "doobie-core" % doobieVersion,
