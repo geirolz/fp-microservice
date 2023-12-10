26a4dc97ad5896a4dcf33534ecf153bdfadd4983
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2023-12-10 00:14:03 +0000

    Update tapir-cats, tapir-core, ... to 1.9.4

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -112,7 +112,7 @@ object Versions {
   val catsVersion: Version           = "2.10.0"
   val catsEffectVersion: Version     = "3.5.1"
   val http4sVersion: Version         = "0.23.23"
-  val tapirVersion: Version          = "1.5.5"
+  val tapirVersion: Version          = "1.9.4"
   val apiSpecOpenapiCirce: Version   = "0.4.0"
   val pureConfigVersion: Version     = "0.17.4"
   val doobieVersion: Version         = "1.0.0-RC2"
