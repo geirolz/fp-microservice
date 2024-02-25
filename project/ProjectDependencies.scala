a87304c16662418257b67692e07d812c5148a612
scala-steward <122865766+scala-steward-geirolz[bot]@users.noreply.github.com>
2024-02-25 14:51:36 +0000

    Update toolkit, toolkit-config, ... to 0.0.12

--- project/ProjectDependencies.scala
+++ project/ProjectDependencies.scala
@@ -107,7 +107,7 @@ object ProjectDependencies {
 
 object Versions {
   type Version = String
-  val geirolzToolkitVersion: Version = "0.0.11"
+  val geirolzToolkitVersion: Version = "0.0.12"
   val scopeVersion: Version          = "0.0.8"
   val catsVersion: Version           = "2.10.0"
   val catsEffectVersion: Version     = "3.5.1"
